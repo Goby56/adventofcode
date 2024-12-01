ff208def6c8d7ad392048ca720dadd47df76af46
Karl Rosengren <karl.rosengren.kr@gmail.com>
2024-12-01 18:33:05 +0100

    Day 1 2024

--- y24/aoc-rust/src/bin/01.rs
+++ y24/aoc-rust/src/bin/01.rs
@@ -1,11 +1,38 @@
 advent_of_code::solution!(1);
 
 pub fn part_one(input: &str) -> Option<u32> {
-    None
+    let mut list1 = vec![];
+    let mut list2 = vec![];
+    for line in input.lines() {
+        let mut ids = line.split_whitespace();
+        list1.push(ids.next().unwrap().parse::<i32>().unwrap());
+        list2.push(ids.next().unwrap().parse::<i32>().unwrap());
+    }
+    list1.sort();
+    list2.sort();
+
+    Some(list1.iter().zip(list2.iter()).fold(0, |dist, (&x, &y)| dist + (x - y).abs()) as u32)
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
-    None
+    let mut list1 = vec![];
+    let mut list2 = vec![];
+    for line in input.lines() {
+        let mut ids = line.split_whitespace();
+        list1.push(ids.next().unwrap().parse::<i32>().unwrap());
+        list2.push(ids.next().unwrap().parse::<i32>().unwrap());
+    }
+    let mut score = 0;
+    for id1 in list1.iter() {
+        let mut count = 0;
+        for id2 in list2.iter() {
+            if id1 == id2 {
+                count += 1;
+            }
+        }
+        score += id1 * count;
+    }
+    Some(score as u32)
 }
 
 #[cfg(test)]
